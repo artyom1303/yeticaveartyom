483518c3f82b308525f5d7426858dcb09f856616
artyom1303 <bokhrenkovr@gmail.com>
2022-06-03 15:56:59 +0300

    save progress (task 12 not completed)

--- sign-up.php
+++ sign-up.php
@@ -11,6 +11,80 @@ if (isset($_SESSION["id"])){
     $avatar = true;
 }
 
+$fields_data = [
+    'email' => NULL,
+    'name' => NULL,
+    'password' => NULL,
+    'contacts' => NULL
+];
+
+$errors = [
+    'email' => '',
+    'name' => '',
+    'password' => '',
+    'contacts' => '',
+    'avatar' => ''
+];
+$all_inputed = true;
+$no_errors = true;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if(   ""==$_POST['email']   ){   $errors['email']='Введите e-mail'; $all_inputed = false;   }
+    if(   ""==$_POST['name']   ){   $errors['name']='Введите имя'; $all_inputed = false;   }
+    if(   ""==$_POST['password']   ){   $errors['password']='Введите пароль'; $all_inputed = false;   }
+    if(   ""==$_POST['contacts']   ){   $errors['contacts']='Напишите как с вами связаться'; $all_inputed = false;   }
+    //if(   true   ){   $errors['avatar']='Загрузите аватар';   }
+    if(   ""==$_FILES['avatar']['name']   ){   $errors['avatar']='Загрузите аватар'; $all_inputed = false;   }
+
+    if ($all_inputed){
+        echo 'all_inputed';
+
+        $connect = new mysqli("localhost","root","","yeticaveartyom");
+
+        $query = "
+SELECT email FROM user_
+WHERE email='".$_POST['email']."'
+";
+        $result = $connect->query($query);
+        if(!$result){
+            die($connect->error." $query");
+        }
+        else{
+            if(mysqli_num_rows($result)>0){
+                $errors['email']='e-mail занят'; $no_errors = false;
+            }
+            else{
+                if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                    $errors['email']='не валидный e-mail'; $no_errors = false;
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        /*
+    $file = $_FILES['avatar']['name'];
+    $path = $_FILES['avatar']['tmp_name'];
+
+    */
+    }
+
+    $fields_data = [
+        'email' => $_POST["email"],
+        'name' => $_POST["name"],
+        'password' => $_POST["password"],
+        'contacts' => $_POST["contacts"]
+    ];
+
+}
 
 
 require_once('functions.php');
@@ -19,11 +93,10 @@ require_once('data.php');
 $main = include_template(
     'sign-up.php',
     [
-        'email_wrong' => $email_wrong,
-        'password_wrong' => $password_wrong,
+        'all_inputed' => $all_inputed,
+        'no_errors' => $no_errors,
+        'errors' => $errors,
         'fields_data' => $fields_data,
-        'email_valid' => $email_valid,
-        'password_valid' => $password_valid,
         'categories' => $categories
     ]
 );
--- templates/sign-up.php
+++ templates/sign-up.php
@@ -13,30 +13,39 @@
         ?>
     </ul>
 </nav>
-<form class="form container form--invalid" action="https://echo.htmlacademy.ru" method="post" autocomplete="off"> <!-- form
-    --invalid -->
+<form class="form container <?=($all_inputed and $no_errors)?'':'form--invalid'?>" action="sign-up.php" method="post" enctype="multipart/form-data" autocomplete="off">
       <h2>Регистрация нового аккаунта</h2>
-      <div class="form__item"> <!-- form__item--invalid -->
+      <div class="form__item <?=$errors['email']==''?'':'form__item--invalid'?>">
         <label for="email">E-mail <sup>*</sup></label>
-        <input id="email" type="text" name="email" placeholder="Введите e-mail">
-        <span class="form__error">Введите e-mail</span>
+        <input id="email" type="text" name="email" placeholder="Введите e-mail" value="<?=$fields_data['email']?>">
+        <span class="form__error"><?=$errors['email']?></span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?=$errors['password']==''?'':'form__item--invalid'?>">
         <label for="password">Пароль <sup>*</sup></label>
-        <input id="password" type="password" name="password" placeholder="Введите пароль">
-        <span class="form__error">Введите пароль</span>
+        <input id="password" type="password" name="password" placeholder="Введите пароль" value="<?=$fields_data['password']?>">
+        <span class="form__error"><?=$errors['password']?></span>
       </div>
-      <div class="form__item">
+      <div class="form__item <?=$errors['name']==''?'':'form__item--invalid'?>">
         <label for="name">Имя <sup>*</sup></label>
-        <input id="name" type="text" name="name" placeholder="Введите имя">
-        <span class="form__error">Введите имя</span>
+        <input id="name" type="text" name="name" placeholder="Введите имя" value="<?=$fields_data['name']?>">
+        <span class="form__error"><?=$errors['name']?></span>
       </div>
-      <div class="form__item">
-        <label for="message">Контактные данные <sup>*</sup></label>
-        <textarea id="message" name="message" placeholder="Напишите как с вами связаться"></textarea>
-        <span class="form__error">Напишите как с вами связаться</span>
+      <div class="form__item <?=$errors['contacts']==''?'':'form__item--invalid'?>">
+        <label for="contacts">Контактные данные <sup>*</sup></label>
+        <textarea id="contacts" name="contacts" placeholder="Напишите как с вами связаться"><?=$fields_data['contacts']?></textarea>
+        <span class="form__error"><?=$errors['contacts']?></span>
       </div>
+    <div class="form__item form__item--file <?=$errors['avatar']==''?'':'form__item--invalid'?>">
+        <label>Аватар <sup>*</sup></label>
+        <div class="form__input-file">
+            <input class="visually-hidden" type="file" id="avatar" name="avatar">
+            <label for="avatar">
+                Добавить
+            </label>
+        </div>
+        <span class="form__error"><?=$errors['avatar']?></span>
+    </div>
       <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
       <button type="submit" class="button">Зарегистрироваться</button>
-      <a class="text-link" href="#">Уже есть аккаунт</a>
+      <a class="text-link" href="login.php">Уже есть аккаунт</a>
 </form>
